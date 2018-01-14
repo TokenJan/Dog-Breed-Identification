3f934138f9a21c6f364f8e5b84fe1fdb33b57025
TokenJan <ichmagmanson@gmail.com>
2018-01-14 16:56:59 +0100

    fix typos

--- utils/merge_standford_data.py
+++ utils/merge_standford_data.py
@@ -1,8 +1,8 @@
 import os
 
-# for folder in os.listdir('../input/Images'):
-#     changed_folder = folder[10:].lower()
-#     os.rename('../input/Images/' + folder, '../input/Images/' + changed_folder)
+for folder in os.listdir('../input/Images'):
+    changed_folder = folder[10:].lower()
+    os.rename('../input/Images/' + folder, '../input/Images/' + changed_folder)
 
-os.rename('../input/test', '../input/test_old')
-os.rename('../input/Images', '../input/test')
+os.rename('../input/train', '../input/train_old')
+os.rename('../input/Images', '../input/train')
